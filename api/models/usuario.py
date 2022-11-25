f5e0303c270d17a177d0ad50e7afe97c555963d8
william9706 <wagnerveleswilliam@gmail.com>
2022-11-24 23:25:38 -0500

    se agrega endpoint y modelo para empresas

--- api/models/usuario.py
+++ api/models/usuario.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 from typing import Optional
 
 # usuario model
 
-class Usuario(BaseModel):
-    UserId: str
-    FirstName: str
-    LastName: str
-    Email:EmailStr
-    YearsPreviousExperience: int
+class Usuarios(BaseModel):
+    UserId: Optional[str]
+    FirstName: str = Field(..., max_Length = 75)
+    LastName: str = Field(max_Length = 75)
+    Email:EmailStr = Field(..., max_Length = 30)
+    YearsPreviousExperience: int 
     Skills: list = []
