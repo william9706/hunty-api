a069c231dcdb3ea974bd1c2da5e6b3bfc4282f8a
william9706 <wagnerveleswilliam@gmail.com>
2022-11-26 01:15:14 -0500

    se agregan test para probar que se cree una vacante y para enlistar vacantes

--- api/test_main.py
+++ api/test_main.py
@@ -1,23 +1,37 @@
 from fastapi.testclient import TestClient
 from .main import app
+from .enums import vacante, vacante_2
+import json
 
 client = TestClient(app)
 
-
-
-
-def test_validar_prueba():
-    response = client.get('/')
+def test_list_vancancy_successful():
+    """  
+    Test para probar que
+    se listen las vacantes
+    """
+    response = client.get('/vacantes')
     assert response.status_code == 200
-    assert response.json() == {"message":"Bienvenido a la api de hunty"}
 
-
-
-def test_read_item():
-    response = client.get("/items/foo", headers={"X-Token": "coneofsilence"})
+def test_save_vancancy_successful():
+    """ 
+    Test para probar que se puede guardar 
+    una vacante satisfactoriamente. 
+    """
+    response = client.post(
+        "/vacantes", 
+        json=vacante
+    )
+    data = response.json()
     assert response.status_code == 200
-    assert response.json() == {
-        "id": "foo",
-        "title": "Foo",
-        "description": "There goes my hero",
-    }
+    assert data["PositionName"] == "Python Dev"
+    assert data == vacante
+
+def test_update_vacancy_successful():
+
+    response = client.put(
+    '/vacantes/ec03955b-3fd8-4261-9eba-1e60b85f5014/', 
+    json=vacante_2
+    )
+    assert response.status_code == 200
+    assert response.json() == vacante_2
